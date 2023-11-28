9047f6bd0a8893ba022927cbe077a66cb0bdd5e8
Sara-pyon <149462367+Sara-pyon@users.noreply.github.com>
2023-11-27 21:36:00 -0700

    build a footer component

--- js/main.js
+++ js/main.js
@@ -1,56 +1,68 @@
 
-// Detail
-const detail = document.querySelector('.js-detail');
-let detailHTML = '';
 
+// Footer
+const footer = document.querySelector('.js-footer__block');
+let footerHTML ='';
 
-const detailFeatures =[{
-    title: 'Super Easy to Use',
-    dicription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat beatae repudiandae unde porro fugiat optio fugit quae quos culpa cumque!',
-    iconName: 'easy',
-    imageName: 'easy'
+const footerFutures = [{
+    title : 'Product',
+    section : ['WordPress Hosting',
+        'Free Automated WordPress',
+        'Migrations',
+        'Domain Names',
+        'Monthly Web Hosting']
 },{
-    title: 'Simply Fast Websites',
-    dicription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat beatae repudiandae unde porro fugiat optio fugit quae quos culpa cumque!',
-    iconName: 'computer',
-    imageName: 'fast'
+    title : 'Company',
+    section : ['About',
+        'Affiliates',
+        'Blog',
+        'Careers',
+        'Community',
+        'News',
+        'Green Hosting']
 },{
-    title: 'Wordpress Made Easy',
-    dicription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat beatae repudiandae unde porro fugiat optio fugit quae quos culpa cumque!',
-    iconName: 'settings',
-    imageName: 'wordpress'
+    title : 'Support',
+    section : ['WordPress Hosting',
+        'Free Automated WordPress',
+        'Migrations',
+        'Domain Names',
+        'Monthly Web Hosting',
+        'DreamCompute']
 },{
-    title: '24/7 Expert Support',
-    dicription: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat beatae repudiandae unde porro fugiat optio fugit quae quos culpa cumque!',
-    iconName: 'clock',
-    imageName: 'support'
+    title : 'Domains',
+    section : ['WordPress Hosting',
+        'Free Automated WordPress',
+        'Migrations',
+        'Domain Names',
+        'Monthly Web Hosting']
 }];
 
-detailFeatures.forEach(item =>{
-    detailHTML += `
-<div class="container grid grid--1x2 detail__item ">
-    <div class="media detail__discription">
-        <div class="media__image">
-            <span class="icon-container">
-                <svg class="icon icon--primary">
-                    <use xlink:href="images/sprite.svg#${item.iconName}"></use>
-                </svg>
-            </span>
-        </div>
-        <div class="media__body">
-            <h2 class="media__title">${item.title}</h2>
-            <p class="media__text">${item.dicription}</p>
-            <a href="#" class="link-arrow">Learn More</a>
-        </div>
+footerFutures.forEach(item => {
+    footerListHTML ='';
+
+    item.section.forEach(list => {
+        footerListHTML += `<list class="nav__item"><a href="#">${list}</a></list>`
+    })
+    
+    footerHTML += `
+    <div class="collapsible">
+                <header class="collapsible__header">
+                    <h2 class="collapsible__heading">${item.title}</h2>
+                    <svg class="icon icon--white collapsible__chevron nav__toggler">
+                        <use xlink:href="images/sprite.svg#chevron"></use>
+                    </svg>
+                </header>
+                <div class="collapsible__content nav__list list">
+                    ${footerListHTML}
+                </div>
     </div>
-    <div class="detail__image">
-        <picture>
-            <source type="image/webp" srcset="images/${item.imageName}.webp 1x, images/${item.imageName}@2x.webp 2x">
-            <source type="image/png" srcset="images/${item.imageName}.jpg 1x, images/${item.imageName}@2x.jpg 2x">
-            <img class="detail__image" src="images/${item.imageName}.jpg" alt="">
-        </picture>
-    </div>
-</div>`
-console.log(detailHTML);
+    `
 });
-detail.innerHTML = detailHTML;
+
+footerHTML += `
+<div class="footer__logo">
+<img class="footer__logo-image" src="images/logo.png" alt="">
+<p class="footer__copyright">Copyright 2024 &copy Sara</p>
+</div>`;
+
+footer.innerHTML = footerHTML;
